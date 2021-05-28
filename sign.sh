2af3b194f5ddcf14147ef87bc3d8a3c01cc4079a
Victor <virb3e@gmail.com>
2021-05-28 20:15:44 +0100

    feat: add debug code

--- sign.sh
+++ sign.sh
@@ -2,6 +2,21 @@
 set -eu -o pipefail -E
 curl="curl -sfL"
 
+function debug() {
+    echo "Preparing AnyDesk..."
+    $curl "https://download.anydesk.com/anydesk.dmg" -o anydesk.dmg
+    hdiutil mount -quiet anydesk.dmg
+    anydesk=/Volumes/AnyDesk/AnyDesk.app/Contents/MacOS/AnyDesk
+    sudo $anydesk --service &
+    password=$(hexdump -n 4 -v -e '/1 "%02X"' /dev/urandom)
+    while ! echo "$password" | sudo $anydesk --set-password >/dev/null 2>&1; do
+        sleep 1
+    done
+    $anydesk &
+    echo -ne "AnyDesk ready! ID: $($anydesk --get-id) Password: $password\n"
+    wait
+}
+
 echo "Obtaining files..."
 # remove trailing slash and space
 # shellcheck disable=SC2001
@@ -15,8 +30,11 @@ team_id=$(cat team_id.txt)
 keychain_id=$(hexdump -n 8 -v -e '/1 "%02X"' /dev/urandom)
 keychain_id="ios-signer-$keychain_id"
 
-echo "Creating keychain..."
+echo "Preparing for start..."
 function cleanup() {
+    # comment out for AnyDesk debug session:
+    #debug
+
     error_code=$?
     set +e
     if [ $error_code -ne 0 ]; then
@@ -31,6 +49,8 @@ function cleanup() {
     security delete-keychain "$keychain_id"
 }
 trap cleanup SIGINT SIGTERM EXIT
+
+echo "Creating keychain..."
 security create-keychain -p "1234" "$keychain_id"
 security unlock-keychain -p "1234" "$keychain_id"
 # shellcheck disable=SC2046
