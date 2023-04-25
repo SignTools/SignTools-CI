c25953ee97c2f1939905098d37be98d79e5eecd1
Kyle Mohr <6644803+kylefmohr@users.noreply.github.com>
2023-04-24 22:17:39 -0500

    fix infinite loop

--- debug.sh
+++ debug.sh
@@ -2,12 +2,16 @@
 set -eu -o pipefail -E
 
 echo "=============================================="
-
 screencapture test.jpg
 echo "Uploading screenshot..."
-until curl https://bashupload.com -T test.jpg; do
+i=10
+until curl https://bashupload.com -T test.jpg || [ $i == 0 ]; do
     echo "Error, sleeping"
     sleep 1
+    i=$((i-1))
 done
 
+if [ $i == 0 ]; then
+    echo "Error uploading screenshot"
+fi
 echo "=============================================="
